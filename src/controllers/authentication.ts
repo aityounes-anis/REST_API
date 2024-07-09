8a8c7ceb860d00fb97dbbd16f0f1349f7c138f5c
unknown <aa.aityounes.anis@gmail.com>
2024-07-09 18:59:11 +0100

    create a login router (/auth/login)

--- src/controllers/authentication.ts
+++ src/controllers/authentication.ts
@@ -1,17 +1,21 @@
 import express from "express";
 
-import { createUser, getUserByEmail } from "actions/users";
-import { authentication, random } from "helpers";
+import { createUser, getUserByEmail } from "../actions/users";
+import { authentication, random } from "../helpers";
 
 export const register = async (req: express.Request, res: express.Response) => {
   try {
     const { email, password, username } = req.body;
 
-    if (!email || !password || !username) res.sendStatus(400);
+    if (!email || !password || !username) {
+      return res.sendStatus(400);
+    }
 
     const existingUser = await getUserByEmail(email);
 
-    if (existingUser) res.sendStatus(400);
+    if (existingUser) {
+      return res.sendStatus(400);
+    }
 
     const salt = random();
 
@@ -24,5 +28,48 @@ export const register = async (req: express.Request, res: express.Response) => {
     return res.status(200).json(user).end();
   } catch (error) {
     console.error(error);
+    return res.sendStatus(500);
+  }
+};
+
+export const login = async (req: express.Request, res: express.Response) => {
+  try {
+    const { email, password } = req.body;
+
+    if (!email || !password) {
+      return res.sendStatus(400);
+    }
+
+    const user = await getUserByEmail(email).select(
+      "+authentication.salt +authentication.password"
+    );
+
+    if (!user) {
+      return res.sendStatus(404);
+    }
+
+    const hashedPassword = authentication(user.authentication.salt, password);
+
+    if (user.authentication.password != hashedPassword) {
+      return res.sendStatus(401);
+    }
+
+    const salt = random();
+    user.authentication.sessionToken = authentication(
+      salt,
+      user._id.toString()
+    );
+
+    await user.save();
+
+    res.cookie("REST-API-AUTH", user.authentication.sessionToken, {
+      domain: "localhost",
+      path: "/",
+    });
+
+    return res.status(200).json(user).end();
+  } catch (error) {
+    console.error(error);
+    return res.sendStatus(500);
   }
 };
