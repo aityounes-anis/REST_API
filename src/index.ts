98a013f8ed909f5a78da3bb8a4e6b1e293d13152
unknown <aa.aityounes.anis@gmail.com>
2024-07-09 16:30:52 +0100

    create user schema

--- src/index.ts
+++ src/index.ts
@@ -4,6 +4,7 @@ import compression from "compression";
 import bodyParser from "body-parser";
 import cookieParser from "cookie-parser";
 import http from "http";
+import mongoose from "mongoose";
 
 const app = express();
 
@@ -22,3 +23,10 @@ const server = http.createServer(app);
 server.listen(8080, () => {
   console.log("SERVER RUNNING ON PORT ", 8080);
 });
+
+const MONGO_URL =
+  "mongodb+srv://aityounes_anis:xY5XLLq18KCD7elb@cluster0.owsn8wn.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0";
+
+mongoose.Promise = Promise;
+mongoose.connect(MONGO_URL);
+mongoose.connection.on("error", (error: Error) => console.error(error));
