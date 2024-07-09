3b77b2bd2829daab2ddaf7ad9b8dd18a979d855c
unknown <aa.aityounes.anis@gmail.com>
2024-07-09 16:24:48 +0100

    Connect to mongoDB

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,24 @@
-console.log("TYPESCRIPT");
+import express from "express";
+import cors from "cors";
+import compression from "compression";
+import bodyParser from "body-parser";
+import cookieParser from "cookie-parser";
+import http from "http";
+
+const app = express();
+
+app.use(
+  cors({
+    credentials: true,
+  })
+);
+
+app.use(compression());
+app.use(cookieParser());
+app.use(bodyParser.json());
+
+const server = http.createServer(app);
+
+server.listen(8080, () => {
+  console.log("SERVER RUNNING ON PORT ", 8080);
+});
